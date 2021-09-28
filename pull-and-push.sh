270ec82669d132713a9a174082228a0a321bedd7
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-09-28 06:52:50 +0530

    new file:   pull-and-push.sh

---
+++ pull-and-push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ex
+
+MAGMA_TAG=1.6.0
+MAGMA_ARTIFACTORY=docker.artifactory.magmacore.org
+
+DOCKER_REGISTRY=magmacore
+
+for i in controller nginx magmalte gateway_python gateway_go cwag_go gateway_pipelined gateway_sessiond operator
+do
+  docker pull ${MAGMA_ARTIFACTORY}/${i}:${MAGMA_TAG}
+  docker tag ${MAGMA_ARTIFACTORY}/${i}:${MAGMA_TAG} ${DOCKER_REGISTRY}/${i}:${MAGMA_TAG}
+  docker push ${DOCKER_REGISTRY}/${i}:${MAGMA_TAG}
+done
