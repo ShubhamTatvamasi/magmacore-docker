09d5956126c241ea29d96b0b6a65fa65824364bf
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-09-14 22:49:28 +0530

    Update pull-and-push.sh

--- pull-and-push.sh
+++ pull-and-push.sh
@@ -7,7 +7,7 @@ NEW_MAGMA_TAG=1.8.0
 MAGMA_ARTIFACTORY=docker.artifactory.magmacore.org
 DOCKER_REGISTRY=magmacore
 
-for i in controller magmalte nginx active-mode-controller configuration-controller radio-controller db-service gateway_go gateway_python agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator
+for i in controller magmalte nginx active-mode-controller configuration-controller radio-controller db-service gateway_go gateway_python agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python agw_gateway_c_arm agw_gateway_python_arm cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator
 do
   docker pull ${MAGMA_ARTIFACTORY}/${i}:${MAGMA_TAG}
   docker tag ${MAGMA_ARTIFACTORY}/${i}:${MAGMA_TAG} ${DOCKER_REGISTRY}/${i}:${NEW_MAGMA_TAG}
